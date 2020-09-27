76e09369a01aa1997392899df4b797b127f326bf
Jyoti <jyoti0005ripu@gmail.com>
2020-09-27 11:20:22 +0530

    created countries component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { CountriesComponent } from './components/countries/countries.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
